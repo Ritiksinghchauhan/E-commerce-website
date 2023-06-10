eed2dd279490a43c3e3b354840ac9ccd3da63e07
Ritiksinghchauhan <101491702+Ritiksinghchauhan@users.noreply.github.com>
2023-06-10 08:41:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const header=document.querySelector("header");
+window.addEventListener("scroll",function(){
+    header.classList.toggle("sticky",this.window.scrollY > 0);
+})
+let menu=document.querySelector('.nav-icon');
+let navmenu=document.querySelector('.nav-menu');
+menu.onclick=() =>{
+    menu.classList.toggle('bx-x');
+    navmenu.classList.toggle('open');
+}
